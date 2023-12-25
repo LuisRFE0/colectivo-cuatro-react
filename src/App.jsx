f1513c51bd3a3a85002b2937e839d1a2fb26a9ce
Luis flores <luis.r.f.e@hotmail.com>
2023-12-25 13:34:22 -0600

    Fusioin

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import { Footer } from "./components/Footer/Footer"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/main.css"
 
-<<<<<<< HEAD
-=======
 export const App = () => {
 
     return (
         <div>
             <Footer />
         </div>
-       )
+    )
 
 }
->>>>>>> c913e6ab195c665fdaaba837ba77785ca820bb0c
