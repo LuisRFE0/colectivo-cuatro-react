8194fb3e64b4bf62407dfe8464cd7edd57ab84a8
Luis flores <luis.r.f.e@hotmail.com>
2023-12-25 12:34:45 -0600

    Header sin rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,38 @@
-
+import '../src/assets/styles/main.css'
+import logo from '../public/images/logo-carita.png'
 export const App = () => {
 
     return (
         <>
             <header>
-
                 <nav className="navbar navbar-expand-lg  navegacion" data-bs-theme="dark">
-
                     <div className="container-fluid">
-
-
                         <a className="color-texto-logo" href="#">
-                            <img src="./assets/images/logo-carita.png" alt="Logo" width="35" height="35"
+                            <img src={logo} alt="Logo" width="35" height="35"
                                 className="d-inline-block align-text-top" />
                             <span className="colectivo">Colectivo Cuatro</span>
                         </a>
-
-
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                             <ul className="navbar-nav">
-
                                 <li className="nav-item">
                                     <a className="nav-link color-texto active" href="#">Inicio</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link color-texto" href="./assets/pages/productos.html"> Productos</a>
+                                    <a className="nav-link color-texto" href="#"> Productos</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link color-texto" href="./assets/pages/contacto.html">Contacto</a>
+                                    <a className="nav-link color-texto" href="#">Contacto</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link color-texto" href="./assets/pages/nosotros.html">Conócenos</a>
-
+                                    <a className="nav-link color-texto" href="#">Conócenos</a>
                                 </li>
                                 <li className="nav-item dropdown">
-                                    <a id="item-1" className="nav-link dropdown-toggle" href="./assets/pages/login.html"
-                                        role="button" aria-expanded="false" style="color: #FAB4C8!important;">
+                                    <a id="item-1" className="nav-link dropdown-toggle" href="#"
+                                        role="button" aria-expanded="false" >
                                         Iniciar sesión
                                     </a>
                                     <ul className="dropdown-menu">
@@ -48,9 +41,8 @@ export const App = () => {
                                     </ul>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link color-texto" href="./assets/pages/cart.html"><i
+                                    <a className="nav-link color-texto" href="#"><i
                                         className="fa-solid fa-cart-shopping"></i></a>
-
                                 </li>
                             </ul>
                         </div>
@@ -60,3 +52,4 @@ export const App = () => {
         </>)
 
 }
+
