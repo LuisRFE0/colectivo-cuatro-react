98d365bfcf69ad1adf4e9ed7c556e01f811521a7
Luis flores <luis.r.f.e@hotmail.com>
2023-12-30 11:27:54 -0600

    Login funcional aun falta comprobar sesion

---
+++ src/assets/js/login/login.js
@@ -0,0 +1,84 @@
+
+export function inciarSesion(user) {
+
+
+    if (validarDatos(user)) {
+        if (obtenerPersona(user)) {
+
+            const sesion = {
+                sesion: true,
+                email: user.correo
+            }
+            const inicio = 'http://localhost:5173/colectivo-cuatro-react';
+
+            localStorage.setItem('sesion', JSON.stringify(sesion));
+            location.href = inicio;
+
+        } else {
+            alertaHtml("Email o contraseña incorrectos", "error");
+
+        }
+    }
+}
+
+function obtenerPersona(persona) {
+    const datosLocalStorage = JSON.parse(localStorage.getItem('usuario'));
+    let respuesta = false;
+    if (datosLocalStorage.some(personas => personas.correo == persona.correo)) {
+        if (datosLocalStorage.some(personas => personas.correo == persona.correo)) {
+            respuesta = true;
+        } else {
+            respuesta = false;
+        }
+    } else {
+        respuesta = false;
+    }
+    return respuesta;
+}
+
+
+
+
+
+function validarDatos(datos) {
+    let respuesta = false;
+
+    if (datos.correo === '') {
+        alertaHtml("Email o contraseña incorrectos", "error");
+        respuesta = false;
+    } else if (datos.contra === '') {
+        alertaHtml("Email o contraseña incorrectos", "error");
+        respuesta = false;
+    } else {
+        respuesta = true;
+    }
+
+    return respuesta
+}
+
+
+function alertaHtml(mensaje, estado) {
+    const alerta = document.querySelector('.alerta');
+    limpiarAlerta();
+    const alertaMensaje = document.createElement('p');
+    if (estado === "error") {
+        alertaMensaje.classList.add('bg-danger', 'text-white', "alerta-error");
+
+    } else {
+        alertaMensaje.classList.add('bg-success', 'text-white', "alerta-error");
+    }
+    alertaMensaje.textContent = mensaje;
+    alerta.appendChild(alertaMensaje);
+
+    setTimeout(() => {
+        alertaMensaje.remove();
+    }, 3000);
+}
+
+
+function limpiarAlerta() {
+    const alerta = document.querySelector('.alerta');
+    while (alerta.firstChild) {
+        alerta.removeChild(alerta.firstChild)
+    }
+}
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,8 +2,23 @@ import { Link } from 'react-router-dom'
 import imgLogin from '../../assets/images/home/Image_login.png'
 import logo from '../../assets/images/home/logo-carita.png';
 import '../../assets/styles/login.css'
+import { inciarSesion } from '../../assets/js/login/login';
+import { useState } from 'react';
 
 export const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    function sesion(e) {
+        e.preventDefault();
+        const user = {
+            correo: email,
+            contra: password
+        }
+        inciarSesion(user);
+    }
 
     return (
         <>
@@ -18,7 +33,7 @@ export const Login = () => {
                         <div className="form-container ">
                             <img src={logo} alt="" />
                             <h3 className="login-welcome color-font color-font-700">Ingresar</h3>
-                            <form action="#" className="form">
+                            <form action="#" className="form" onSubmit={sesion}>
                                 <div className="mb-3 row">
                                     <label
                                         htmlFor="staticEmail"
@@ -28,6 +43,9 @@ export const Login = () => {
                                     </label>
                                     <div className="col-sm-10">
                                         <input
+                                            onChange={function (e) {
+                                                setEmail(e.target.value);
+                                            }}
                                             type="text"
                                             className="form-control"
                                             id="inputEmail"
@@ -44,15 +62,19 @@ export const Login = () => {
                                     </label>
                                     <div className="col-sm-10">
                                         <input
+                                            onChange={function (e) {
+                                                setPassword(e.target.value);
+                                            }}
                                             type="password"
                                             className="form-control"
                                             id="inputPassword"
                                         />
                                     </div>
                                 </div>
-                                <div className="alerta text-center" />
+                                <div className="alerta text-center"></div>
+
                                 <div className=" btns-login mt-5">
-                                    <button className="btn-login">Ingresar</button>
+                                    <button type='submit' className="btn-login">Ingresar</button>
                                     <a className="mt-4" href="#">
                                         Recuperar contraseña
                                     </a>
